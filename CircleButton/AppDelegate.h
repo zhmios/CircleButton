6025133a08f07d7e25e392a0394ebe4e351928ff
Zhang Haiming <zhanghaiming@keegoo.com>
2014-09-17 11:17:26 +0800

    Initial Commit

---
+++ CircleButton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CircleButton
+//
+//  Created by user on 14-9-17.
+//  Copyright (c) 2014年 jianguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CircleButtonTests/CircleButtonTests.m
@@ -0,0 +1,34 @@
+//
+//  CircleButtonTests.m
+//  CircleButtonTests
+//
+//  Created by user on 14-9-17.
+//  Copyright (c) 2014年 jianguo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CircleButtonTests : XCTestCase
+
+@end
+
+@implementation CircleButtonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
